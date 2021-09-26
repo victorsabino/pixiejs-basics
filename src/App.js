58a3ff94e2a75ad5e324129d9eee999e95115b37
Victor Sabino <victorsabino6@gmail.com>
2021-09-26 04:27:59 -0300

    react pixiejs basics

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import PixiGame from "./components/PixiGame"
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Pixi App in ReactJs</h1>
+      <PixiGame />
+    </div>
+  );
+}
